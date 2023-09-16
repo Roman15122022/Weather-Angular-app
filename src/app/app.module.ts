cf2de452bb420193728cdba3c392dc2f8421a982
Roman15122022 <90318946+romanchik787@users.noreply.github.com>
2023-09-17 01:25:59 +0300

    fix all problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,39 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { WeatherService } from "./Service/weather.service";
+import { WidgetComponent } from './widget/widget.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WidgetComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatListModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
