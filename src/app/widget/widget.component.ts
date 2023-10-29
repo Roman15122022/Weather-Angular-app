9eff71f71d8da1181d11ea40e95f33192d379f80
Roman15122022 <mallrom1231@gmail.com>
2023-10-30 01:33:20 +0200

    feat: use slick

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -6,6 +6,7 @@ import {WIDGET_STORAGE_KEY, WidgetService} from "../services/widgetservice/widge
 import {interval} from "rxjs";
 import {map, switchMap} from "rxjs/operators";
 
+
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
@@ -83,10 +84,37 @@ export class WidgetComponent implements OnInit {
 
   visibleWidget = this.weatherWidgets.slice();
   nextSlide() {
+
   }
 
   prevSlide() {
 
+  }
+  slides = [
+    { img: 'https://via.placeholder.com/600.png/09f/fff' },
+    { img: 'https://via.placeholder.com/600.png/021/fff' },
+    { img: 'https://via.placeholder.com/600.png/321/fff' },
+    { img: 'https://via.placeholder.com/600.png/422/fff' },
+    { img: 'https://via.placeholder.com/600.png/654/fff' },
+  ];
+  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
+  addSlide() {
+    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+  }
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
   }
 
 }
